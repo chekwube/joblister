f3d6b52e010ce83a93559387ce8dd0a11315ff45
Cheks <okekechekwube1@gmail.com>
2018-09-17 22:18:37 +0100

    Add files via upload

---
+++ helpers/system_helper.php
@@ -0,0 +1,48 @@
+<?php 
+	//Redirect To Page
+	function redirect($page = FALSE, $message = NULL, $message_type = NULL){
+		if(is_string ($page)){
+			$location = $page;
+		}else{
+			$location = $_SERVER['SCRIPT_NAME'];
+		}
+
+		//Check for Message
+		if($message != NULL){
+			//Set Message
+			$_SESSION['message'] = $message;
+		}
+
+		//Check for Type
+		if($message_type != NULL){
+			//Set Message Type
+			$_SESSION['message_type'] = $message_type;
+		}
+
+		//Redirect
+		header('Location: '.$location);
+		exit;
+	}
+
+	function displayMessage(){
+		if(!empty($_SESSION['message'])){
+			//Assign Message Var
+			$message = $_SESSION['message'];
+
+			if(!empty($_SESSION['message_type'])){
+				//Assign type var
+				$message_type = $_SESSION['message_type'];
+				//Create Output
+				if($message_type == 'error'){
+					echo '<div class="alert alert-danger">'.$message.'</div>';
+				}else{
+					echo '<div class="alert alert-success">'.$message.'</div>';
+				}
+			}
+			unset($_SESSION['message']);
+			unset($_SESSION['message_type']);
+		}else{
+			echo '';
+		}
+	}
+?>
